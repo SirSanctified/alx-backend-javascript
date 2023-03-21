ebf057ed7476401edd46817d56a5ac1ba8f63090
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-21 14:41:37 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
+  if (trueOrFalse) { /* empty */ }
 
   return [task, task2];
 }
