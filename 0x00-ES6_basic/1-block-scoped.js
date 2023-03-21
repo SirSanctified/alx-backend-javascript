76804c43b5d0c386107fb1f073d651dda1e6a647
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-21 14:52:52 +0200

    Emulate variable hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) { /* empty */ }
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+  let task = false;
+  let task2 = true;
 
   return [task, task2];
 }
