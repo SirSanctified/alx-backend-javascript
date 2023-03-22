466f49ab5d1d0e5e12a4bf86671afff2937e0340
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-22 16:08:37 +0200

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/extensions
+import { uploadPhoto, createUser } from './util.js';
+
+export default function handleProfileSignup() {
+  uploadPhoto().then((response) => {
+    createUser().then((user) => {
+      console.log(`${response.body} ${user.firstName} ${user.lastName}`);
+    });
+  });
+}
