573fdad82ac4734035c20510136b0185707ab2d0
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-28 11:35:49 +0200

    Verify if set is an instance of Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  if (typeof startString === 'string' && startString.length > 0) {
+  if (typeof startString === 'string' && startString.length > 0 && set instanceof Set) {
     set.forEach((value) => {
       if (value.startsWith(startString)) {
         result += `${value.slice(startString.length)}-`;
