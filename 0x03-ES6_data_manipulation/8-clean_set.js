2b74914e32fd2deb20e389225c2b707728cd2191
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-28 10:46:10 +0200

    Remove the toString call

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,8 @@ export default function cleanSet(set, startString) {
   const result = [];
   if (startString !== '') {
     set.forEach((element) => {
-      const elem = element.toString();
-      if (elem.startsWith(startString)) {
-        result.push(elem.slice(startString.length));
+      if (element.startsWith(startString)) {
+        result.push(element.slice(startString.length));
       }
     });
   }
