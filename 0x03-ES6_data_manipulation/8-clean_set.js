5004721dd72357a3d498725d91c197433f6d25aa
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-28 11:24:19 +0200

    Make sure the length of startString is > 0

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      result += `${value.slice(startString.length)}-`;
-    }
-  });
+  if (startString.length > 0) {
+    set.forEach((value) => {
+      if (value.startsWith(startString)) {
+        result += `${value.slice(startString.length)}-`;
+      }
+    });
+  }
   return result.slice(0, -1);
 }
