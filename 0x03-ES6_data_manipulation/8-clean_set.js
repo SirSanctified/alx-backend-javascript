f1e5967f9dc2694be7636e1fc1db41b224f5e99d
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-28 11:32:36 +0200

    Verify if startString is a string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  if (startString.length > 0) {
+  if (typeof startString === 'string' && startString.length > 0) {
     set.forEach((value) => {
       if (value.startsWith(startString)) {
         result += `${value.slice(startString.length)}-`;
