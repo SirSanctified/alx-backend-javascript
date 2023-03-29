b31ccdde994b6f511d9b8425d0990a7b98d09f11
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-29 14:46:26 +0200

    Create the StudentClass class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,25 @@ function printTeacher(firstName: string, lastName: string): string {
 interface printTeacherFunction {
     (firstName: string, lastName: string): string; 
 }
+
+interface Student {
+    firstName: string,
+    lastName: string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+    constructor(firstName:string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
